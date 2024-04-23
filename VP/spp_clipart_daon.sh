b267330dae0e1a29dcbca0547ab3575d6a875d74
GingerCohle <37873318+GingerCohle@users.noreply.github.com>
2024-04-23 05:32:59 -0700

    Update spp_clipart_daon.sh

--- VP/spp_clipart_daon.sh
+++ VP/spp_clipart_daon.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python tools/train_net_da.py   --config-file configs/sigma_plus_plus/pascal_to_clipart_res101.yaml
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_da.py   --config-file configs/sigma_plus_plus/pascal_to_clipart_res101.yaml
